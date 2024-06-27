152c348afcff6527b04d0aad12f1fea23e852f77
ademitidayo <adenijibimm08@gmail.com>
2024-06-27 00:06:16 -0700

    strain

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """This module defines a class to manage file storage for hbnb clone"""
 import json
+import unittest
 
 
 class FileStorage:
@@ -45,6 +46,6 @@ class FileStorage:
             with open(FileStorage.__file_path, 'r') as f:
                 temp = json.load(f)
                 for key, val in temp.items():
-                        self.all()[key] = classes[val['__class__']](**val)
+                    self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
